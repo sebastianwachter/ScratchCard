d8f43fcb9ebd4ce74ef5fd1624642522e3ec87a8
Sebastian Wachter <sebi.wachter@online.de>
2015-10-12 21:13:31 +0200

    add touch support

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,52 @@ var lastX, lastY;
 var ctx;
 
 function initThis() {
-  ctx = document.getElementById('myCanvas').getContext('2d');
-
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.fillStyle = "rgb(163, 163, 163)";
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-  $('#myCanvas').mousedown(function (e) {
+  can = document.getElementById('myCanvas');
+  ctx = can.getContext('2d');
+  
+  can.addEventListener("mousedown", mouseDown, false);
+  can.addEventListener("mousemove", mouseMove, false);
+  can.addEventListener("touchstart", touchDown, false);
+  can.addEventListener("touchmove", touchMove, true);
+  can.addEventListener("touchend", touchUp, false);
+  
+  document.body.addEventListener("mouseup", mouseUp, false);
+  document.body.addEventListener("touchcancel", touchUp, false);
+  
+  reset();
+  
+  function mouseDown(e) {
     mousePressed = true;
     Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-  });
-
-  $('#myCanvas').mousemove(function (e) {
+  }
+  
+  function mouseMove(e) {
     if(mousePressed) {
-      Draw(e.pageX - $(this).offset().left, e.pageY -$(this).offset().top, true);
+      Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
     }
-  });
-
-  $('#myCanvas').mouseup(function (e) {
+  }
+  
+  function mouseUp(e) {
     mousePressed = false;
-  });
+  }
+  
+  function touchDown(e) {
+    e.preventDefault();
+    mousePressed = true;
+    Draw(e.targetTouches[0].pageX - $(this).offset().left, e.targetTouches[0].pageY - $(this).offset().top, false);
+  }
+  
+  function touchMove(e) {
+    e.preventDefault();
+    if(mousePressed) {
+      Draw(e.targetTouches[0].pageX - $(this).offset().left, e.targetTouches[0].pageY - $(this).offset().top, true);
+    }
+  }
+  
+  function touchUp(e) {
+    //e.preventDefault();
+    mousePressed = false;
+  }
 }
 
 function Draw(x, y, isDown) {
@@ -39,4 +65,10 @@ function Draw(x, y, isDown) {
   }
   lastX = x;
   lastY = y;
+}
+
+function reset() {
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.fillStyle = "rgb(163, 163, 163)";
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
