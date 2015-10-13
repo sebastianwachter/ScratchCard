48764f38acad4f708b27b70b9ef8eca97139f728
Sebastian Wachter <wachter@hydra-newmedia.com>
2015-10-13 10:42:36 +0200

    add a callback of how much area is freed fix #2

--- scripts/script.js
+++ scripts/script.js
@@ -5,48 +5,47 @@ var ctx;
 function initThis() {
   can = document.getElementById('myCanvas');
   ctx = can.getContext('2d');
-  
+
   can.addEventListener("mousedown", mouseDown, false);
   can.addEventListener("mousemove", mouseMove, false);
   can.addEventListener("touchstart", touchDown, false);
   can.addEventListener("touchmove", touchMove, true);
   can.addEventListener("touchend", touchUp, false);
-  
+
   document.body.addEventListener("mouseup", mouseUp, false);
   document.body.addEventListener("touchcancel", touchUp, false);
-  
+
   reset();
-  
+
   function mouseDown(e) {
     mousePressed = true;
     Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
   }
-  
+
   function mouseMove(e) {
     if(mousePressed) {
       Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
     }
   }
-  
+
   function mouseUp(e) {
     mousePressed = false;
   }
-  
+
   function touchDown(e) {
     e.preventDefault();
     mousePressed = true;
     Draw(e.targetTouches[0].pageX - $(this).offset().left, e.targetTouches[0].pageY - $(this).offset().top, false);
   }
-  
+
   function touchMove(e) {
     e.preventDefault();
     if(mousePressed) {
       Draw(e.targetTouches[0].pageX - $(this).offset().left, e.targetTouches[0].pageY - $(this).offset().top, true);
     }
   }
-  
+
   function touchUp(e) {
-    //e.preventDefault();
     mousePressed = false;
   }
 }
@@ -65,10 +64,33 @@ function Draw(x, y, isDown) {
   }
   lastX = x;
   lastY = y;
+  handlePercentage(getClearedPixels(25));
 }
 
 function reset() {
   ctx.globalCompositeOperation = 'source-over';
   ctx.fillStyle = "rgb(163, 163, 163)";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function getClearedPixels(stride) {
+  if (!stride || stride < 1) { stride = 1; }
+
+  var pixels = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height),
+      pdata = pixels.data,
+      l = pdata.length,
+      total = (l / stride),
+      count = 0;
+
+  for(var i = count = 0; i < l; i += stride) {
+    if (parseInt(pdata[i]) === 0) {
+      count++;
+    }
+  }
+  return Math.round((count / total) * 100);
+}
+
+function handlePercentage(clearedPixels) {
+  clearedPixels = clearedPixels || 0;
+  console.log('cleared: ' + clearedPixels + '%');
 }
